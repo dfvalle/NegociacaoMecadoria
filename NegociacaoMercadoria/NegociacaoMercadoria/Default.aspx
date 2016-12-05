ac77a4ab1279f38b07cc33583cea7263da996e82
dfvalle <danielfdovalle@gmail.com>
2016-12-05 00:50:30 -0200

    Add files via upload

---
+++ NegociacaoMercadoria/NegociacaoMercadoria/Default.aspx
@@ -0,0 +1,173 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NegociacaoMercadoria.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Negociação de Mercadoria
+    </title>
+
+    <script src="Scripts/jquery-3.1.1.min.js"></script>
+    <script src="Scripts/jquery.maskMoney.min.js"></script>
+
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+
+
+    <script type="text/javascript">
+
+
+
+        $(function () {
+
+            $('#codigoMercadoriaTextBox').maskMoney({
+                showSymbol: true,
+                symbol: "",
+                decimal: "",
+                thousands: ""
+            });
+
+            $('#quantidadeTextBox').maskMoney({
+                showSymbol: true,
+                symbol: "",
+                decimal: "",
+                thousands: ""
+            });
+
+
+            $('#precoTextBox').maskMoney({
+                showSymbol: true,
+                symbol: "R$",
+                decimal: ",",
+                thousands: "."
+            });
+        });
+
+    </script>
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+
+            <header>
+                <div style="text-align: center">
+                    <h2>Negociação de Mercadoria </h2>
+                </div>
+            </header>
+
+            <main>
+                <div class="container" style="text-align: center">
+                    <asp:Label ID="mensagemLabel" runat="server" Font-Bold="True"></asp:Label>
+                </div>
+
+                <asp:MultiView ID="negociacaoMultiView" runat="server" ActiveViewIndex="0">
+                    <asp:View ID="listaView" runat="server" ViewStateMode="Enabled">
+                        <div class="container">
+                            <asp:GridView CssClass="form-control"
+                                ID="listaGridView"
+                                AutoGenerateColumns="False"
+                                DataKeyNames="Id"
+                                runat="server"
+                                OnRowCommand="listaGridView_RowCommand1" 
+                                AllowPaging="True" 
+                                OnPageIndexChanging="listaGridView_PageIndexChanging" 
+                                PageSize="5">
+                                <Columns>
+                                    <asp:BoundField DataField="codigoMercadoria"
+                                        ItemStyle-CssClass=""
+                                        HeaderText="Código"
+                                        HeaderStyle-CssClass="" />
+                                    <asp:ButtonField DataTextField="nomeMercadoria"
+                                        ItemStyle-CssClass=""
+                                        HeaderStyle-CssClass=""
+                                        CommandName="alterar"
+                                        HeaderText="Nome da Mercadoria" />
+                                </Columns>
+                            </asp:GridView>
+                            <br />
+                            <asp:Button ID="novoButton" runat="server" Text="Novo" CssClass="btn btn-primary" OnClick="novoButton_Click" />
+
+                        </div>
+                    </asp:View>
+                    <asp:View ID="dadosView" runat="server">
+                        <div class="container">
+
+
+
+                            <div class="form-group">
+                                <asp:Label ID="codigoMercadoriaLabel" runat="server" Text="Código da Mercadoria:"></asp:Label>
+                                <asp:TextBox ID="codigoMercadoriaTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+
+                            </div>
+                            <br />
+
+
+                            <div class="form-group">
+                                <asp:Label ID="tipoMercadoriaLabel" runat="server" Text="Tipo da Mercadoria:"></asp:Label>
+                                <asp:TextBox ID="tipoMercadoriaTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+                            </div>
+                            <br />
+
+                            <div class="form-group">
+                                <asp:Label ID="nomeMercadoriaLabel" runat="server" Text="Nome da Mercadoria:"></asp:Label>
+                                <asp:TextBox ID="nomeMercadoriaTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+                            </div>
+                            <br />
+
+                            <div class="form-group">
+                                <asp:Label ID="quantidadeLabel" runat="server" Text="Quantidade:"></asp:Label>
+                                <asp:TextBox ID="quantidadeTextBox" runat="server" CssClass="form-control" onkeyup="javascript:formataInteiro(this,event);"></asp:TextBox>
+                            </div>
+                            <br />
+
+                            <div class="form-group">
+                                <asp:Label ID="precoLabel" runat="server" Text="Preço"></asp:Label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">R$</div>
+                                    <asp:TextBox ID="precoTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+                                </div>
+                            </div>
+                            <br />
+
+                            <div class="form-group">
+                                <asp:Label ID="tipoNegocioLabel" runat="server" Text="Tipo de Negocio"></asp:Label>
+                                <asp:DropDownList ID="tipoNegocioDropDownList" runat="server" CssClass="form-control">
+                                    <asp:ListItem>Compra</asp:ListItem>
+                                    <asp:ListItem>Venda</asp:ListItem>
+                                </asp:DropDownList>
+                            </div>
+                            <br />
+
+                            <asp:Button ID="salvarButton" runat="server" Text="Salvar" CssClass="btn btn-primary" OnClick="salvarButton_Click" />
+                            <asp:Button ID="excluirButton" runat="server" Text="Excluir" OnClientClick="return confirm (&quot;Deseja excluir registro ?&quot;);" CssClass="btn btn-primary" />
+                            <asp:Button ID="voltarButton" runat="server" Text="Voltar" CssClass="btn btn-primary" OnClick="voltarButton_Click" />
+
+                            <br />
+                            <br />
+
+                            <asp:GridView ID="lsitaGridView" runat="server"></asp:GridView>
+
+
+
+                        </div>
+                    </asp:View>
+                </asp:MultiView>
+
+
+
+
+
+
+            </main>
+
+
+            <footer>
+                <div>
+                </div>
+            </footer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ NegociacaoMercadoria/NegociacaoMercadoria/Default.aspx.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace NegociacaoMercadoria
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public string conexao = ConfigurationManager.ConnectionStrings["conexao"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Page.IsPostBack)
+                    carregarLista();
+
+            }
+            catch (Exception erro)
+            {
+                this.mensagemLabel.Text = erro.Message;
+            }
+
+        }
+
+        protected void novoButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //exbir os campos para entrada de dados controle dadosView
+                this.negociacaoMultiView.ActiveViewIndex = 1;
+                this.mensagemLabel.Text = string.Empty;
+            }
+            catch (Exception erro)
+            {
+
+                this.mensagemLabel.Text = erro.Message;
+            }
+        }
+
+        protected void voltarButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //exbir a visão da lista de dados controle listaView
+                this.negociacaoMultiView.ActiveViewIndex = 0;
+                this.mensagemLabel.Text = string.Empty;
+                this.carregarLista();
+            }
+            catch (Exception erro)
+            {
+
+                this.mensagemLabel.Text = erro.Message;
+            }
+        }
+
+        protected void salvarButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //salvar banco de dados
+                this.salvar();
+                
+            }
+            catch (Exception erro)
+            {
+
+                this.mensagemLabel.Text = erro.Message;
+            }
+        }
+
+        protected void excluirButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.excluir();
+            }
+            catch (Exception erro)
+            {
+
+                this.mensagemLabel.Text = erro.Message;
+            }
+        }
+
+
+        protected void listaGridView_PageIndexChanging(object sender, System.Web.UI.WebControls.GridViewPageEventArgs e)
+        {
+            try
+            {
+                listaGridView.PageIndex = e.NewPageIndex;
+                carregarLista();
+            }
+            catch (Exception erro)
+            {
+                this.mensagemLabel.Text = erro.Message;
+            }
+        }
+
+        protected void listaGridView_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            try
+            {
+                var gv = (GridView)sender;
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+                int id = (int)gv.DataKeys[rowIndex].Value;
+                if (  this.pesquisar(id) == false )
+                    throw new Exception("Mercadoria não cadastrada.");
+                else
+                    this.negociacaoMultiView.ActiveViewIndex = 1;
+
+            }
+            catch (Exception erro)
+            {
+                this.mensagemLabel.Text = erro.Message;
+            }
+        }
+
+        private void salvar()
+        {
+
+            //Definir a conexão
+            var con = new SqlConnection(conexao);
+
+            //Definir o comando
+            var sql = @"SELECT 
+                        codigoMercadoria, 
+                        tipoMercadoria,
+                        nomeMercadoria,
+                        quantidade,
+                        preco,
+                        tipoNegocio  
+                        FROM NegociacaoMercadoria 
+                        WHERE codigoMercadoria=@codigoMercadoria";
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@codigoMercadoria", this.codigoMercadoriaTextBox.Text);
+
+            //Abrir conexão
+            con.Open();
+
+            //Definir o leitor
+            var leitor = cmd.ExecuteReader();
+
+            if (leitor.Read() == false)
+                this.incluir();
+            else
+                this.alterar();
+        }
+
+
+        private void incluir()
+        {
+            //Definir a conexão
+            var con = new SqlConnection(conexao);
+
+            //Definir o comando
+            var sql = @"INSERT INTO NegociacaoMercadoria 
+                        (
+                        codigoMercadoria,
+                        tipoMercadoria,
+                        nomeMercadoria,
+                        quantidade,
+                        preco,
+                        tipoNegocio
+                        )
+                        VALUES (
+                        @codigoMercadoria,
+                        @tipoMercadoria,
+                        @nomeMercadoria,
+                        @quantidade,
+                        @preco,
+                        @tipoNegocio
+                        )";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@codigoMercadoria", this.codigoMercadoriaTextBox.Text);
+            cmd.Parameters.AddWithValue("@tipoMercadoria", this.tipoMercadoriaTextBox.Text);
+            cmd.Parameters.AddWithValue("@nomeMercadoria", this.nomeMercadoriaTextBox.Text);
+            cmd.Parameters.AddWithValue("@quantidade", this.quantidadeTextBox.Text);
+            cmd.Parameters.AddWithValue("@preco", this.precoTextBox.Text.Replace(".", "").Replace(",", "."));
+            cmd.Parameters.AddWithValue("@tipoNegocio", this.tipoNegocioDropDownList.Text);
+
+
+            //Abrir conexão
+            con.Open();
+
+            //Executar comando
+            cmd.ExecuteNonQuery();
+
+            //Fechar conexão
+            con.Close();
+
+            this.codigoMercadoriaTextBox.Text = string.Empty;
+            this.tipoMercadoriaTextBox.Text = string.Empty;
+            this.nomeMercadoriaTextBox.Text = string.Empty;
+            this.quantidadeTextBox.Text = string.Empty;
+            this.precoTextBox.Text = string.Empty;
+            this.tipoNegocioDropDownList.SelectedIndex = 0;
+
+            this.mensagemLabel.Text = "Inclusão efetuada com sucesso";
+        }
+
+        private void alterar()
+        {
+            //Definir a conexão
+            var con = new SqlConnection(conexao);
+
+            //Definir o comando
+            var sql = @"UPDATE NegociacaoMercadoria 
+                        SET
+                        tipoMercadoria=@tipoMercadoria
+                        nomeMercadoria=@nomeMercadoria
+                        quantidade=@quantidade
+                        preco=@preco
+                        tipoNegocio=@tipoNegocio
+                        WHERE
+                        codigoMercadoria=@codigoMercadoria
+                        ";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@codigoMercadoria", this.codigoMercadoriaTextBox.Text);
+            cmd.Parameters.AddWithValue("@tipoMercadoria", this.tipoMercadoriaTextBox.Text);
+            cmd.Parameters.AddWithValue("@nomeMercadoria", this.nomeMercadoriaTextBox.Text);
+            cmd.Parameters.AddWithValue("@quantidade", this.quantidadeTextBox.Text);
+            cmd.Parameters.AddWithValue("@preco", this.precoTextBox.Text.Replace(".", "").Replace(",", "."));
+            cmd.Parameters.AddWithValue("@tipoNegocio", this.tipoNegocioDropDownList.Text);
+
+
+            //Abrir conexão
+            con.Open();
+
+            //Executar comando
+            cmd.ExecuteNonQuery();
+
+            //Fechar conexão
+            con.Close();
+                       
+            this.mensagemLabel.Text = "Inclusão efetuada com sucesso";
+        }
+
+        private void excluir()
+        {
+            //Definir a conexão
+            var con = new SqlConnection(conexao);
+
+            //Definir o comando
+            var sql = @"DELETE FROM NegociacaoMercadoria 
+                      WHERE  codigoMercadoria=@codigoMercadoria";
+
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@codigoMercadoria", this.codigoMercadoriaTextBox.Text);
+           
+            //Abrir conexão
+            con.Open();
+
+            //Executar comando
+            cmd.ExecuteNonQuery();
+
+            //Fechar conexão
+            con.Close();
+
+            this.mensagemLabel.Text = "Inclusão efetuada com sucesso";
+        }
+
+        private bool pesquisar(int codigoMercadoria)
+        {
+            //Definir a conexão
+            var con = new SqlConnection(conexao);
+
+            //Definir o comando
+            var sql = @"SELECT 
+                        codigoMercadoria, 
+                        tipoMercadoria,
+                        nomeMercadoria,
+                        quantidade,
+                        preco,
+                        tipoNegocio  
+                        FROM NegociacaoMercadoria 
+                        WHERE codigoMercadoria=@codigoMercadoria";
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@codigoMercadoria", codigoMercadoria);
+
+            //Abrir conexão
+            con.Open();
+
+            //Definir o leitor
+            var leitor = cmd.ExecuteReader();
+
+            if ( leitor.Read() ==  false )
+            {
+                this.codigoMercadoriaTextBox.Text = string.Empty;
+                this.tipoMercadoriaTextBox.Text = string.Empty;
+                this.nomeMercadoriaTextBox.Text = string.Empty;
+                this.quantidadeTextBox.Text = string.Empty;
+                this.precoTextBox.Text = string.Empty;
+                this.tipoNegocioDropDownList.SelectedIndex = 0;
+                //Fechar conexão
+                con.Close();
+                return false;
+            }
+            else
+            {
+                this.codigoMercadoriaTextBox.Text = leitor["codigoMercadoria"].ToString();
+                this.tipoMercadoriaTextBox.Text = leitor["tipoMercadoria"].ToString();
+                this.nomeMercadoriaTextBox.Text = leitor["nomeMercadoria"].ToString();
+                this.quantidadeTextBox.Text = leitor["quantidade"].ToString();
+                this.precoTextBox.Text = leitor["preco"].ToString();
+                this.tipoNegocioDropDownList.SelectedValue = leitor["codigoMercadoria"].ToString();
+            }
+
+            //Fechar conexão
+            con.Close();
+
+            return true;
+
+        }
+
+        private void carregarLista()
+        {
+            try
+            {
+                //Definir comando SQL
+                var sql = @"SELECT 
+                        codigoMercadoria, 
+                        tipoMercadoria,
+                        nomeMercadoria,
+                        quantidade,
+                        preco,
+                        tipoNegocio  
+                        FROM NegociacaoMercadoria";
+
+                //DataAdapter
+                var da = new SqlDataAdapter(sql, conexao);
+
+                //DataSet 
+                var ds = new DataSet();
+
+                //Preencher o DataSet
+                da.Fill(ds);
+
+                //Vincular o DataSet ao GridView
+                this.listaGridView.DataSource = ds;
+                this.listaGridView.DataBind();
+
+            }
+            catch (Exception erro)
+            {
+                Response.Write(erro.Message);
+            }
+
+        }
+
+    }
+
+
+
+
+}
